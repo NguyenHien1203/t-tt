d928e291d71f621e16902d2794c1a77cb99e05a2
minhtd19196 <148860021+minhtd19196@users.noreply.github.com>
2023-10-27 10:55:42 +0700

    minhtd check_dm_rt

--- src/app/demo/components/danh-muc/danh-muc.module.ts
+++ src/app/demo/components/danh-muc/danh-muc.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DanhMucRoutingModule } from './danh-muc-routing.module';
-import { RouterModule } from '@angular/router';
-import { ChucDanhComponent } from './chuc-danh/chuc-danh.component';
-import { NotfoundComponent } from '../notfound/notfound.component';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-          children: [
-              { path: 'chuc-danh', loadChildren: () => import('./chuc-danh/chuc-danh-routing.module').then(m => m.ChucDanhRoutingModule) },
-              { path: 'linh-vuc', loadChildren: () => import('./linh-vuc/linh-vuc.component').then(m => m.LinhVucComponent) },
-            ]
-      },
-      { path: 'notfound', component: NotfoundComponent },
-      { path: '**', redirectTo: '/notfound' },
-  ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' }),
-CommonModule,
+    CommonModule,
     DanhMucRoutingModule
   ]
 })

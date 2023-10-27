07948956ab969546fc6aa058515a31bfa459873c
minhtd19196 <148860021+minhtd19196@users.noreply.github.com>
2023-10-27 17:32:11 +0700

    minhtd edit meun

---
+++ src/app/demo/models/mail.ts
@@ -0,0 +1,4 @@
+export interface Mail {
+    label : string ;
+    createdDate : Date;
+}
--- src/app/layout/app.topbar.component.ts
+++ src/app/layout/app.topbar.component.ts
@@ -1,16 +1,20 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { LayoutService } from "./service/app.layout.service";
-
+import { Mail } from '../demo/models/mail';
+import { Notifi } from '../demo/models/notifi';
+import { Clock } from '../demo/models/clock';
 @Component({
     selector: 'app-topbar',
     templateUrl: './app.topbar.component.html'
 })
 export class AppTopBarComponent implements OnInit {
     DonVi_NhomQuyen: MenuItem[] = [];
-
+    products: any[] = [];
     items!: MenuItem[];
-
+    mails: Mail[] = [];
+    notifis: Notifi[] = [];
+    clocks: Clock[] = [];
     @ViewChild('menubutton') menuButton!: ElementRef;
 
     @ViewChild('topbarmenubutton') topbarMenuButton!: ElementRef;
@@ -19,9 +23,26 @@ export class AppTopBarComponent implements OnInit {
 
     constructor(public layoutService: LayoutService) { }
     ngOnInit(): void {
+        
+        this.mails = [
+            { label: 'Nhận thông báo', createdDate: new Date(1901,10,10) },
+            { label: 'Nhân viên (Bảo vệ)', createdDate: new Date(1901,1,10) }
+        ];
+
+        this.notifis = [
+            { label: 'Notifi', createdDate: new Date(1901,10,10) },
+            { label: 'Thông báo tiền lương ', createdDate: new Date(1901,1,10) }
+        ];
+
+        this.clocks = [
+            { label: 'Sắp họp', createdDate: new Date(1901,10,10) },
+            { label: 'Mai họp', createdDate: new Date(1901,1,10) }
+        ];
+
         this.DonVi_NhomQuyen = [
             { label: 'Văn thư (CNTT)', icon: '', url: '' },
             { label: 'Nhân viên (Bảo vệ)', icon: '', routerLink: ['/theming'] }
         ];
     }
 }
+

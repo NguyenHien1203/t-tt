a74b5dd88f256a7c3b1d78f70bc65fab47a8df4c
minhtd19196 <148860021+minhtd19196@users.noreply.github.com>
2023-10-27 10:50:53 +0700

    minhtd change danh-muc-rt

--- src/app/demo/components/danh-muc/danh-muc.module.ts
+++ src/app/demo/components/danh-muc/danh-muc.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DanhMucRoutingModule } from './danh-muc-routing.module';
-import { DanhBaComponent } from './danh-ba/danh-ba.component';
+import { RouterModule } from '@angular/router';
+import { ChucDanhComponent } from './chuc-danh/chuc-danh.component';
+import { NotfoundComponent } from '../notfound/notfound.component';
 
 @NgModule({
   imports: [
-    CommonModule,
+    RouterModule.forRoot([
+      {
+          children: [
+              { path: 'chuc-danh', loadChildren: () => import('./chuc-danh/chuc-danh-routing.module').then(m => m.ChucDanhRoutingModule) },
+              { path: 'linh-vuc', loadChildren: () => import('./linh-vuc/linh-vuc.component').then(m => m.LinhVucComponent) },
+            ]
+      },
+      { path: 'notfound', component: NotfoundComponent },
+      { path: '**', redirectTo: '/notfound' },
+  ], { scrollPositionRestoration: 'enabled', anchorScrolling: 'enabled', onSameUrlNavigation: 'reload' }),
+CommonModule,
     DanhMucRoutingModule
-  ],
-  declarations: [
-    DanhBaComponent
   ]
 })
 export class DanhMucModule { }

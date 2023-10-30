359e463632f6a07c71c3993872d094df64439e63
TuanDinh <tuanphama8a@gmail.com>
2023-10-30 09:00:41 +0700

    Tuan Upgrade

--- src/app/demo/components/danh-muc/danh-muc-routing.module.ts
+++ src/app/demo/components/danh-muc/danh-muc-routing.module.ts
@@ -7,7 +7,6 @@ import { RouterModule } from '@angular/router';
     { path: 'chuc-danh', data: { breadcrumb: 'Button' }, loadChildren: () => import('./chuc-danh/chuc-danh.module').then(m => m.ChucDanhModule) },
     { path: 'danh-ba', data: { breadcrumb: 'Button' }, loadChildren: () => import('./danh-ba/danh-ba.module').then(m => m.DanhBaModule) },
     { path: 'phong-ban', data: { breadcrumb: 'Button' }, loadChildren: () => import('./phong-ban/phong-ban.module').then(m => m.PhongBanModule) },
-    { path: 'chuc-danh', data: { breadcrumb: 'Chức Danh' }, loadChildren: () => import('./chuc-danh/chuc-danh.module').then(m => m.ChucDanhModule) },
     { path: 'lien-ket', data: { breadcrumb: 'Liên Kết' }, loadChildren: () => import('./lien-ket/lien-ket.module').then(m => m.LienKetModule) },
   ])],
   exports: [RouterModule]

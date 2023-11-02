75a6fe52bf8ccbf54ec5a51603ec599918925646
TuanDinh <tuanphama8a@gmail.com>
2023-11-02 15:41:03 +0700

    TuanCapNhat Commit

--- src/app/demo/components/danh-muc/phong-ban/cap-nhat-phong-ban/cap-nhat-phong-ban.component.ts
+++ src/app/demo/components/danh-muc/phong-ban/cap-nhat-phong-ban/cap-nhat-phong-ban.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { DmPhongBan } from 'src/app/demo/api/danh-muc/phong-ban';
+import { PhongbanService } from 'src/app/demo/service/danh-muc/phong-ban/phongban.service';
+import { DMJsonModel } from 'src/app/models/common/DMJsonModel';
 
 @Component({
   selector: 'app-cap-nhat-phong-ban',
   templateUrl: './cap-nhat-phong-ban.component.html',
   styleUrls: ['./cap-nhat-phong-ban.component.scss']
 })
-export class CapNhatPhongBanComponent {
+export class CapNhatPhongBanComponent implements OnInit {
+
+  submitted = false;
+  DonViTree: DMJsonModel[] = [];
+  selectedDonVi: '';
+  productDialog: boolean = false;
+  IdDonVi: any;
+  Cap: any;
+  MaDVPC: any;
+  checkHt: boolean = false;
+
+  public formCapNhat = this.formBuilder.group({
+    id: [0, []],
+    maDonVi: ["", []],
+    tenDonVi: ["", []],
+    parentId: [0, []],
+    url: ["", []],
+    ngayTruyenThong: [new Date, []],
+    diaChi: ["", []],
+    ghiChu: ["", []],
+    sdt: ["", []],
+    fax: ["", []],
+    website: ["", []],
+    email: ["", []],
+    thuTu: ["", []],
+    trangThai: [false, []],
+  });
+
+  constructor(private route: ActivatedRoute, private phongbanService: PhongbanService, private formBuilder: FormBuilder) { }
+  @Input() id: string = '';
+  @Input() hienThi: boolean = false;
+  @Output() tatPopup = new EventEmitter<boolean>();
+
+  ngOnInit(): void {
+    this.GetDataDonVi();
+  }
+
+  public BindDataDialog(): void {
+    this.phongbanService.GetPhongBanById(this.id).subscribe(data => {
+      console.log(data)
+      let iteam = data;
+      this.formCapNhat.setValue(iteam);
+    })
+  }
+
+  public Thoat(): void {
+    this.hienThi = false;
+    this.tatPopup.emit(this.hienThi);
+  }
+
+  onSelectChange(event: any) {
+    this.IdDonVi = event.id;
+    this.Cap = event.cap;
+  }
+
+  public CheckHienThi(): void {
+    this.checkHt = !this.checkHt;
+  }
+
+  public GetDataDonVi() {
+    this.phongbanService.GetDataDonVi().subscribe(data => {
+      if (data.isError) {
+        console.log("Dữ liệu không hợp lệ")
+      } else {
+        this.DonViTree = this.transformJsonToCustomStructure(data.objData)
+      }
+    }, (error) => {
+      console.log('Error', error);
+    })
+  }
+
+  transformJsonToCustomStructure(jsonData: any): DMJsonModel[] {
+    const customData: DMJsonModel[] = [];
+
+    // Biến đổi dữ liệu JSON thành cấu trúc dữ liệu tùy chỉnh
+    // Ví dụ: jsonData có dạng [{ id: 1, name: 'Node 1', children: [...] }, ...]
+
+    for (const item of jsonData) {
+      const customNode: DMJsonModel = {
+        id: item.id,
+        cap: item.cap,
+        label: item.title,
+        children: []
+      };
+
+      if (item.subs && item.subs.length > 0) {
+        customNode.children = this.transformJsonToCustomStructure(item.subs);
+      }
+
+      customData.push(customNode);
+    }
+
+    return customData;
+  }
 
 }
--- src/app/demo/components/danh-muc/phong-ban/phong-ban-routing.module.ts
+++ src/app/demo/components/danh-muc/phong-ban/phong-ban-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { PhongBanComponent } from './phong-ban.component';
 import { RouterModule } from '@angular/router';
+import { CapNhatPhongBanComponent } from './cap-nhat-phong-ban/cap-nhat-phong-ban.component';
 
 @NgModule({
   imports: [RouterModule.forChild([
       {path: '', component: PhongBanComponent} ,
 
-      {path: 'phong-ban/cap-nhat-phong-ban/:id', component: PhongBanComponent} 
+      {path: 'phong-ban/cap-nhat-phong-ban/:id', component: CapNhatPhongBanComponent} 
       
     ])],
     exports: [RouterModule]

9a4c1c782599c1dbc4207861b5df5024ebf7ce50
minhtd19196 <148860021+minhtd19196@users.noreply.github.com>
2023-11-02 15:41:24 +0700

    minhtd thongbao

--- src/app/demo/components/thong-tin-khac/quan-ly-thong-bao/them-moi/them-moi.component.ts
+++ src/app/demo/components/thong-tin-khac/quan-ly-thong-bao/them-moi/them-moi.component.ts
@@ -14,80 +14,94 @@ import { UploadFileService } from 'src/app/demo/service/upload-file.service';
   styleUrls: ['./them-moi.component.scss']
 })
 export class ThemMoiComponent {
-  uploadFiles : any = [];
+  uploadFiles: any;
+  pathFileUpload: string = '';
+  public checkHienThi : boolean = false;
   public Editor = ClassicEditor;
   @Input() hienThi: boolean = false;
   @Output() tatPopup = new EventEmitter<boolean>();
   public loaiNhiemvu: any = {};
-  public submitted : boolean  = false;
+  public submitted: boolean = false;
   public formThemMoi = this.fb.group({
     id: [0, []],
     tieuDe: ["", [Validators.required]],
-    tuNgay: ["", []],
-    denNgay: ["", []],
+    ngayBatDau: ["", []],
+    ngayKetThuc: ["", []],
     donViId: [0, []],
-    noiDung : ["", []],
-    hienThi : [false, []]
+    noiDung: ["", []],
+    hienThi: [false, []]
   });
   constructor(private fb: FormBuilder
-    , private service : LoaiNhiemVuService
-    ,private messageService: MessageService
-    ,private authService : AuthService
-    ,private fileService: UploadFileService){ }
+    , private service: LoaiNhiemVuService
+    , private messageService: MessageService
+    , private authService: AuthService
+    , private fileService: UploadFileService) { }
 
   public Thoat(): void {
-  this.hienThi = false;
-  this.formThemMoi.reset();
-  this.tatPopup.emit(this.hienThi);
-}
-
-onUpload(event) {
-  for (let file of event.files) {
-    this.uploadFiles = file;
+    this.hienThi = false;
+    this.formThemMoi.reset();
+    this.tatPopup.emit(this.hienThi);
   }
-  this.messageService.add({severity: 'info', summary : 'Tệp đã tải lên' , detail : ''});
-}
 
+  onUpload(event) {
+    for (let file of event.files) {
+      this.uploadFiles = file;
+    }
+    this.fileService.postFile(this.uploadFiles).subscribe(data => {
+      if (data.isError) {
+        this.messageService.add({ severity: 'error', summary: 'Lỗi', detail: data.title });
+      } else {
+        this.pathFileUpload = data.data;
+        this.messageService.add({ severity: 'info', summary: 'Thành công', detail: data.title });
+      }
+    }, error => {
+      console.log(error);
+    });
 
-public ThemMoi(): void {
-  this.submitted = true;
-  if (this.formThemMoi.valid) { 
-    this.loaiNhiemvu = this.formThemMoi.value;
-    this.loaiNhiemvu.donViId = this.authService.GetDonViLamViec();
-    this.loaiNhiemvu.createdBy = this.authService.GetmUserInfo()?.userId;
-    console.log(this.loaiNhiemvu)
-
-    this.service.themMoiLoaiNhiemVu(this.loaiNhiemvu).subscribe(
-      data => {
-        console.log('data', data);
-        let resData = data as ResponeMessage;
-        if (resData.isError) {
-          this.messageService.add({ severity: 'error', summary: 'Error', detail: resData.title });
-        } else {
-          this.messageService.add({ severity: 'success', summary: 'Success', detail: resData.title });
-          this.Thoat();
-        }
-      },
-      error => {
-        console.log('Error', error);
-      })
   }
-}
 
-public getToolBar(): any {
-  const toolbar: any = {
-    items: [
-      'undo', 'redo',
-      '|', 'heading',
-      '|', 'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
-      '|', 'bold', 'italic', 'strikethrough', 'subscript', 'superscript', 'code',
-      '|', 'link', 'uploadImage', 'blockQuote', 'codeBlock',
-      '|', 'alignment',
-      '|', 'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent'
-    ],
-    shouldNotGroupWhenFull: true
+  public ThemMoi(): void {
+    this.submitted = true;
+    if (this.formThemMoi.valid) {
+      this.loaiNhiemvu = this.formThemMoi.value;
+      this.loaiNhiemvu.donViId = this.authService.GetDonViLamViec();
+      this.loaiNhiemvu.createdBy = this.authService.GetmUserInfo()?.userId;
+
+      this.service.themMoiLoaiNhiemVu(this.loaiNhiemvu).subscribe(
+        data => {
+          console.log('data', data);
+          let resData = data as ResponeMessage;
+          if (resData.isError) {
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: resData.title });
+          } else {
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: resData.title });
+            this.Thoat();
+          }
+        },
+        error => {
+          console.log('Error', error);
+        })
+    }
   }
-  return toolbar;
-}
+
+  public getToolBar(): any {
+    const toolbar: any = {
+      items: [
+        'undo', 'redo',
+        '|', 'heading',
+        '|', 'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
+        '|', 'bold', 'italic', 'strikethrough', 'subscript', 'superscript', 'code',
+        '|', 'link', 'uploadImage', 'blockQuote', 'codeBlock',
+        '|', 'alignment',
+        '|', 'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent'
+      ],
+      shouldNotGroupWhenFull: true
+    }
+    return toolbar;
+  }
+
+  public CheckedHt(): void {
+    this.checkHienThi = !this.checkHienThi;
+  }
 
 }

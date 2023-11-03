d10f75ac9a6ce5dd7db39d29b92356efd56a34d5
minhtd19196 <148860021+minhtd19196@users.noreply.github.com>
2023-11-03 17:29:22 +0700

    minhtd upload file tb

--- src/app/demo/components/thong-tin-khac/quan-ly-thong-bao/them-moi/them-moi.component.ts
+++ src/app/demo/components/thong-tin-khac/quan-ly-thong-bao/them-moi/them-moi.component.ts
@@ -7,6 +7,7 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { UploadFileService } from 'src/app/demo/service/upload-file.service';
 import { QuanLyThongBaoService } from 'src/app/demo/service/thong-tin-khac/quan-ly-thong-bao.service';
 import { throwError } from 'rxjs';
+import { ChangeDetectorRef } from '@angular/core';
 
 
 @Component({
@@ -16,21 +17,22 @@ import { throwError } from 'rxjs';
 })
 export class ThemMoiComponent {
   file: File | null = null; // Variable to store file
-  public checkHienThi: boolean = false;
-  public Editor = ClassicEditor;
+   checkHienThi: boolean = false;
+   Editor = ClassicEditor;
   @ViewChild('myEditor') myEditor: any;
   @Input() show: boolean = false;
   @Output() tatPopup = new EventEmitter<boolean>();
-  public quanLyThongBao: any = {};
-  public submitted: boolean = false;
-  public formThemMoi = this.fb.group({
+   quanLyThongBao: any = {};
+   submitted: boolean = false;
+   relativePath : string = "";
+   formThemMoi = this.fb.group({
     id: [0, []],
     tieuDe: ["", [Validators.required]],
-    ngayBatDau: ["", []],
-    ngayKetThuc: ["", []],
+    ngayBatDau: [, []],
+    ngayKetThuc:[, []],
     donViId: [0, []],
     noiDung: ["", []],
-    hienThi: [false, []],
+    hienThi: [, []],
     fileName: ["", []],
     filePath: ["", []],
   });
@@ -38,12 +40,14 @@ export class ThemMoiComponent {
     , private service: QuanLyThongBaoService
     , private messageService: MessageService
     , private authService: AuthService
-    , private fileService: UploadFileService) { }
+    , private fileService: UploadFileService
+    , private cd: ChangeDetectorRef) { }
 
   public Thoat(): void {
     this.show = false;
     this.formThemMoi.reset();
     this.tatPopup.emit(this.show);
+    this.cd.detectChanges();
   }
 
   onChange(event: any) {
@@ -52,11 +56,18 @@ export class ThemMoiComponent {
     if (file) {
       this.file = file;
       this.fileService.uploadFile(this.file).subscribe({
-        next: () => {
-          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Tải lên thành công' });
+        next: (data) => {
+          if (data.isError)
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Tải lên thất bại' });
+          else {
+            this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Tải lên thành công' });
+            this.formThemMoi.value.fileName = data.objData.fileName;
+            this.formThemMoi.value.filePath = data.objData.filePath;
+            this.relativePath = data.objData.filePath;
+          }
         },
         error: (error: any) => {
-          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Tải lên thất bại' });
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Có lỗi xảy ra' });
           return throwError(() => error);
         },
       });;
@@ -75,6 +86,7 @@ export class ThemMoiComponent {
       this.service.themMoiQuanLyThongBao(this.quanLyThongBao).subscribe(
         data => {
           console.log('data', data);
+          this.XoaFile();
           let resData = data as ResponeMessage;
           if (resData.isError) {
             this.messageService.add({ severity: 'error', summary: 'Error', detail: resData.title });
@@ -107,5 +119,9 @@ export class ThemMoiComponent {
 
   public CheckedHt(): void {
     this.checkHienThi = !this.checkHienThi;
+  }
+
+  public XoaFile(): void {
+    this.file = null;
   }
 }

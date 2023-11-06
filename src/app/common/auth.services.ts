464ccc0cc582e1da1818571029250862ca563990
NguyenHien1203 <120232630+NguyenHien1203@users.noreply.github.com>
2023-11-06 09:10:17 +0700

    hiennv

--- src/app/common/auth.services.ts
+++ src/app/common/auth.services.ts
@@ -4,52 +4,58 @@ import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { mUserInfo } from '../models/common/mUserInfo';
 @Injectable({
-    providedIn: 'root'
-  })
-  export class AuthService {
-    constructor(private router: Router,private cookieService: CookieService) { }
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private router: Router, private cookieService: CookieService) { }
 
-    public CheckLogin(): boolean {
-        let status = false;
-        if (this.cookieService.get('isLoggedIn') == "true") {
-          if (this.CheckTokenExpired(this.GetToken())) {
-            status = false;
-            this.cookieService.set('isLoggedIn', 'false');
-            this.cookieService.delete('token');
-            this.cookieService.delete('mUserInfo');
-            this.cookieService.delete('idDonViLamViec');
-          } else {
-            status = true;
-          }
-        }
-        else {
-          status = false;
-        }
-        return status;
+  public CheckLogin(): boolean {
+    let status = false;
+    if (this.cookieService.get('isLoggedIn') == "true") {
+      if (this.CheckTokenExpired(this.GetToken())) {
+        status = false;
+        this.cookieService.set('isLoggedIn', 'false');
+        this.cookieService.delete('token');
+        this.cookieService.delete('mUserInfo');
+        this.cookieService.delete('idDonViLamViec');
+      } else {
+        status = true;
       }
-
-      public CheckTokenExpired(token: string) {
-        const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
-        return (Math.floor((new Date).getTime() / 1000)) >= expiry;
-      }
-
-      public GetToken(): string {
-        let token = this.cookieService.get('token');
-        if (token != null) {
-          return token;
-        } else {
-          return "";
-        }
-      }
-
-      public GetmUserInfo() : mUserInfo {
-        const mUser : mUserInfo = JSON.parse(this.cookieService.get('mUserInfo'));
-        return mUser;
-      }
-
-      public GetDonViLamViec() : string {
-        const DonViLamViec  = this.cookieService.get('idDonViLamViec');
-        return DonViLamViec;
-      }
-      
+    }
+    else {
+      status = false;
+    }
+    return status;
   }
+
+  public CheckTokenExpired(token: string) {
+    const expiry = (JSON.parse(atob(token.split('.')[1]))).exp;
+    return (Math.floor((new Date).getTime() / 1000)) >= expiry;
+  }
+
+  public GetToken(): string {
+    let token = this.cookieService.get('token');
+    if (token != null) {
+      return token;
+    } else {
+      return "";
+    }
+  }
+
+  public GetmUserInfo(): mUserInfo {
+    if (this.cookieService.get('mUserInfo')) {
+      const mUser: mUserInfo = JSON.parse(this.cookieService.get('mUserInfo'));
+      return mUser;
+    }
+    return null;
+  }
+
+  public GetDonViLamViec(): string {
+    if (this.cookieService.get('idDonViLamViec')) {
+      const DonViLamViec = this.cookieService.get('idDonViLamViec');
+      return DonViLamViec;
+    }
+    return null;
+  }
+
+}

b0033e50d5244ada4c9e45f5e653d1cf1831e00d
TuanDinh <tuanphama8a@gmail.com>
2023-11-16 09:14:39 +0700

    Tuan commit

--- src/app/demo/components/danh-muc/nhom-don-vi/them-don-vi-vao-nhom/them-don-vi-vao-nhom.component.ts
+++ src/app/demo/components/danh-muc/nhom-don-vi/them-don-vi-vao-nhom/them-don-vi-vao-nhom.component.ts
@@ -1,8 +1,10 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { NhomDonViService } from 'src/app/demo/service/danh-muc/nhom-don-vi/nhom-don-vi.service';
+import { DMJsonModel } from 'src/app/models/common/DMJsonModel';
+import {TimKiemModel } from 'src/app/models/danh-muc/don-vi/donvi.model';
 
 @Component({
   selector: 'app-them-don-vi-vao-nhom',
@@ -24,6 +26,71 @@ export class ThemDonViVaoNhomComponent {
   
   submitted = false;
   NhomDonVi: any = {};
-
+  TenNhomDonVi: string;
   
+    //Khai báo đơn vị tree
+    DonViTree: DMJsonModel[] = [];
+    selectedDonVi: '';
+
+   //#region Khai báo form thêm mới và dữ liệu
+   public FormNhomDonVi = this.formBuilder.group({
+    id: [0, []],
+    tenNhom: ["", []],
+  });
+  //#endregion
+
+  //#region bind dữ liệu cần chỉnh sửa
+  public BindDataDialog(): void {
+    this.Service.GetNhomDonViById(this.id).subscribe(data => {
+      this.FormNhomDonVi.setValue(data);
+      var formvalue = this.FormNhomDonVi.value;
+      this.TenNhomDonVi = formvalue.tenNhom;
+    })
+  }
+  //#endregion
+  
+  //Hàm đóng pop up
+  public Thoat(): void {
+    this.hienThi = false;
+    this.submitted = false;
+    this.tatPopup.emit(this.hienThi);
+  }
+
+  public GetDataDonVi() {
+    this.Service.GetDataDonVi().subscribe(data => {
+      console.log(data)
+      if (data.isError) {
+        console.log("Dữ liệu không hợp lệ")
+      } else {
+        this.DonViTree = this.transformJsonToCustomStructure(data.objData)
+        console.log(this.DonViTree)
+      }
+    }, (error) => {
+      console.log('Error', error);
+    })
+  }
+
+  transformJsonToCustomStructure(jsonData: any): DMJsonModel[] {
+    const customData: DMJsonModel[] = [];
+
+    // Biến đổi dữ liệu JSON thành cấu trúc dữ liệu tùy chỉnh
+    // Ví dụ: jsonData có dạng [{ id: 1, name: 'Node 1', children: [...] }, ...]
+
+    for (const item of jsonData) {
+      const customNode: DMJsonModel = {
+        id: item.id,
+        cap:item.cap,
+        label: item.title,
+        children: []
+      };
+
+      if (item.subs && item.subs.length > 0) {
+        customNode.children = this.transformJsonToCustomStructure(item.subs);
+      }
+
+      customData.push(customNode);
+    }
+
+    return customData;
+  }
 }

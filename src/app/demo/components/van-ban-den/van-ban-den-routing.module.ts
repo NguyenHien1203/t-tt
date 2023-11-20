f3a907e1074d9d52c479008f4529f5b381e79cec
NguyenHien1203 <120232630+NguyenHien1203@users.noreply.github.com>
2023-11-20 09:11:50 +0700

    hiennv

--- src/app/demo/components/van-ban-den/van-ban-den-routing.module.ts
+++ src/app/demo/components/van-ban-den/van-ban-den-routing.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
     { path: 'cap-nhat-moi', data: { breadcrumb: 'Button' }, loadChildren: () => import('./cap-nhat-moi/cap-nhat-moi.module').then(m => m.CapNhatMoiModule) },
     { path: 'them-moi', data: { breadcrumb: 'Button' }, loadChildren: () => import('./cap-nhat-moi/them-moi/them-moi.module').then(m => m.ThemMoiModule) },
     { path: 'tiep-nhan-van-ban', data: { breadcrumb: 'Button' }, loadChildren: () => import('./tiep-nhan-van-ban/tiep-nhan-van-ban.module').then(m => m.TiepNhanVanBanModule) },
+    { path: 'but-phe-van-ban', data: { breadcrumb: 'Button' }, loadChildren: () => import('./but-phe-van-ban/but-phe-van-ban.module').then(m => m.ButPheVanBanModule) },
   ])],
   exports: [RouterModule]
 })

d06f520f2b66eb7142cf5086a65c65c0e6b56c4b
saemunity <huanhoang2092000@gmail.com>
2023-12-09 11:41:42 +0700

    Update

--- src/app/demo/components/thong-tin-khac/hoat-dong-sap-toi/them-moi/them-moi.component.ts
+++ src/app/demo/components/thong-tin-khac/hoat-dong-sap-toi/them-moi/them-moi.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { AuthService } from 'src/app/common/auth.services';
+import { HoatDongSapToiService } from 'src/app/demo/service/thong-tin-khac/hoat-dong-sap-toi/hoat-dong-sap-toi.service';
 
 @Component({
   selector: 'app-them-moi',
@@ -12,19 +13,58 @@ export class ThemMoiComponent implements OnInit {
   @Input() show: boolean = false;
   @Output() close = new EventEmitter<boolean>;
 
-  todayDate:Date = new Date();
-
+  todayDate: Date = new Date();
+  public formThemMoi = this.formBuilder.group({
+    id: [0, []],
+    noiDung: ["", []],
+    ngayDienRa: [Date, []],
+    donViId: [0, []],
+    created: [new Date(), []],
+    createdBy: [0, []],
+    lastModified: [new Date(), []],
+    lastModifiedBy: [0, []],
+  });
+  valueForm: any;
   submitted = false;
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private messageService: MessageService) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private messageService: MessageService,
+    private hoatDongSapToiService: HoatDongSapToiService,
+  ) { }
 
   ngOnInit(): void {
-      
+
+  }
+
+  ThemMoi() {
+    this.submitted = true;
+    this.valueForm = this.formThemMoi.value;
+    this.valueForm.donViId = this.authService.GetDonViLamViec();
+    this.valueForm.create = this.authService.GetmUserInfo().userId;
+    this.valueForm.lastModifiedBy = this.authService.GetmUserInfo().userId;
+
+    if (this.formThemMoi.valid) {
+      this.submitted = true;
+      this.hoatDongSapToiService.themMoi(this.formThemMoi.value).subscribe(
+        data => {
+          let resData = data;
+          if (resData.isError) {
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: resData.title });
+          } else {
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: resData.title });
+          }
+        });
+      this.TatPopup();
+      this.formThemMoi.reset();
+    }
   }
 
   TatPopup() {
     this.submitted = false;
     this.show = false;
     this.close.emit(this.show);
+    this.formThemMoi.reset();
   }
 }

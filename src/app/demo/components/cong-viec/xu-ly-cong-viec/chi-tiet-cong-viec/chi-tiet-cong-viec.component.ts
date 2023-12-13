cbf614e1c9140e2852b0454c6af7ad2ae1a8d3ca
NguyenHien1203 <120232630+NguyenHien1203@users.noreply.github.com>
2023-12-13 10:17:09 +0700

    hiennv

--- src/app/demo/components/cong-viec/xu-ly-cong-viec/chi-tiet-cong-viec/chi-tiet-cong-viec.component.ts
+++ src/app/demo/components/cong-viec/xu-ly-cong-viec/chi-tiet-cong-viec/chi-tiet-cong-viec.component.ts
@@ -5,79 +5,81 @@ import { XuLyCongViecService } from 'src/app/demo/service/cong-viec/xu-ly-cong-v
 import { ChiTietCongViec } from 'src/app/models/cong-viec/xu-ly-cong-viec';
 
 @Component({
-  selector: 'app-chi-tiet-cong-viec',
-  templateUrl: './chi-tiet-cong-viec.component.html',
-  styleUrls: ['./chi-tiet-cong-viec.component.scss']
+    selector: 'app-chi-tiet-cong-viec',
+    templateUrl: './chi-tiet-cong-viec.component.html',
+    styleUrls: ['./chi-tiet-cong-viec.component.scss']
 })
 export class ChiTietCongViecComponent {
-  @Input() id: string = '1';
-  @Input() cap: string = '1';
-  @Input() loai: string = '1';
-  @Input() show: boolean = false;
-  @Output() tatPopup = new EventEmitter<boolean>();
+    @Input() id: string = '1';
+    @Input() cap: string = '1';
+    @Input() loai: string = '1';
+    @Input() show: boolean = false;
+    @Output() tatPopup = new EventEmitter<boolean>();
 
-  constructor(
-      private service: XuLyCongViecService,
-      private messageService: MessageService,
-      private authService: AuthService
-  ) {}
+    TenTaiKhoan: string = this.authService.GetmUserInfo().userName;
 
-  chiTietCongViecs: ChiTietCongViec = {
-      soKiHieu: '',
-      trichYeu: '',
-      noiDungCongViec: '',
-      nguoiXuLy: '',
-      tuNgay: new Date(),
-      denNgay: new Date(),
-      thoiGianDuKien: '',
-      chiDao: '',
-      chuTri: '',
-      lstDongChiPhoiHop: [],
-      lstDongChiThongBao: [],
-      lstFileDinhKemVanBan: [],
-      lstFileDinhKemCongViec: [],
-      lstCaNhanXuLyCongViec: [],
-  };
+    constructor(
+        private service: XuLyCongViecService,
+        private messageService: MessageService,
+        private authService: AuthService
+    ) { }
 
-  loading: boolean = true;
-  submitted: boolean = false;
-  lstHoSoCongViec: any[] = [];
-  lstTrangThai: any[] = [
-      { text: 'Đang xử lý', value: 2 },
-      { text: 'Xử lý xong', value: 3 },
-  ];
-  idDonViLamViec: string = this.authService.GetDonViLamViec() ?? '0';
+    chiTietCongViecs: ChiTietCongViec = {
+        soKiHieu: '',
+        trichYeu: '',
+        noiDungCongViec: '',
+        nguoiXuLy: '',
+        tuNgay: new Date(),
+        denNgay: new Date(),
+        thoiGianDuKien: '',
+        chiDao: '',
+        chuTri: '',
+        lstDongChiPhoiHop: [],
+        lstDongChiThongBao: [],
+        lstFileDinhKemVanBan: [],
+        lstFileDinhKemCongViec: [],
+        lstCaNhanXuLyCongViec: [],
+    };
 
-  ngOnInit() {
-      this.loading = false;
-  }
+    loading: boolean = true;
+    submitted: boolean = false;
+    lstHoSoCongViec: any[] = [];
+    lstTrangThai: any[] = [
+        { text: 'Đang xử lý', value: 2 },
+        { text: 'Xử lý xong', value: 3 },
+    ];
+    idDonViLamViec: string = this.authService.GetDonViLamViec() ?? '0';
 
-  public async BindDialogData() {
-      try {
-          const data = await this.service.getDataViewChiTiet(
-              this.id,
-              this.cap,
-              this.loai
-          );
-          console.log(data);
-          if (data != null) {
-              this.chiTietCongViecs = data;
-          }
-      } catch (error) {
-          console.log(error);
-      }
-  }
+    ngOnInit() {
+        this.loading = false;
+    }
 
-  public ChangeHoSoCongViec(event) {}
+    public async BindDialogData() {
+        try {
+            const data = await this.service.getDataViewChiTiet(
+                this.id,
+                this.cap,
+                this.loai
+            );
 
-  public Thoat(): void {
-      this.lstHoSoCongViec = [];
-      this.submitted = false;
-      this.show = false;
-      this.tatPopup.emit(this.show);
-  }
+            if (data != null) {
+                this.chiTietCongViecs = data;
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-  public ChonHoSo() {
-      this.submitted = true;
-  }
+    public ChangeHoSoCongViec(event) { }
+
+    public Thoat(): void {
+        this.lstHoSoCongViec = [];
+        this.submitted = false;
+        this.show = false;
+        this.tatPopup.emit(this.show);
+    }
+
+    public ChonHoSo() {
+        this.submitted = true;
+    }
 }

e0038bdb91e72b7064847ac3463a82750aca1a54
saemunity <huanhoang2092000@gmail.com>
2023-12-14 10:35:40 +0700

    Update

--- src/app/demo/components/ky-so/trinh-ky-van-ban/trinh-ky-van-ban-routing.module.ts
+++ src/app/demo/components/ky-so/trinh-ky-van-ban/trinh-ky-van-ban-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TrinhKyVanBanComponent } from './trinh-ky-van-ban.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: TrinhKyVanBanComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/demo/components/ky-so/trinh-ky-van-ban/trinh-ky-van-ban.module.ts
+++ src/app/demo/components/ky-so/trinh-ky-van-ban/trinh-ky-van-ban.module.ts
@@ -1,14 +1,60 @@
+import { TrinhKyVanBanComponent } from './trinh-ky-van-ban.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TrinhKyVanBanRoutingModule } from './trinh-ky-van-ban-routing.module';
+import { ThemMoiComponent } from './them-moi/them-moi.component';
+import { CapNhatComponent } from './cap-nhat/cap-nhat.component';
 
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { CalendarModule } from 'primeng/calendar';
+import { ChipsModule } from 'primeng/chips';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputTextModule } from 'primeng/inputtext';
+import { Select2Module } from 'ng-select2-component';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ThemMoiComponent,
+    CapNhatComponent,
+    TrinhKyVanBanComponent
+  ],
   imports: [
     CommonModule,
-    TrinhKyVanBanRoutingModule
+    TrinhKyVanBanRoutingModule,
+
+    ConfirmDialogModule,
+    FileUploadModule,
+    DialogModule,
+    FormsModule,
+    Select2Module,
+    AutoCompleteModule,
+    CalendarModule,
+    ChipsModule,
+    DropdownModule,
+    InputMaskModule,
+    InputNumberModule,
+    CascadeSelectModule,
+    MultiSelectModule,
+    InputTextareaModule,
+    InputTextModule,
+    CheckboxModule,
+    TableModule,
+    ToastModule,
+    BreadcrumbModule,
   ]
 })
 export class TrinhKyVanBanModule { }

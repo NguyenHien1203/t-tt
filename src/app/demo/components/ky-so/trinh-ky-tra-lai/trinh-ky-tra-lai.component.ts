0df562c41b445bfd2c43e4892f86a882831b77f6
saemunity <huanhoang2092000@gmail.com>
2023-12-20 10:05:21 +0700

    Update

---
+++ src/app/demo/components/ky-so/trinh-ky-tra-lai/trinh-ky-tra-lai.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trinh-ky-tra-lai',
+  templateUrl: './trinh-ky-tra-lai.component.html',
+  styleUrls: ['./trinh-ky-tra-lai.component.scss']
+})
+export class TrinhKyTraLaiComponent {
+
+}

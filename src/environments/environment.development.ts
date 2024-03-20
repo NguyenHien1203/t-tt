7040236b4d1ea23c861a7969eace6da13f9e73e3
NguyenHien1203 <120232630+NguyenHien1203@users.noreply.github.com>
2024-03-20 15:57:48 +0700

    hiennv

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,7 @@
 export const environment = {
-   baseUrlApi: 'https://localhost:7077/api'
+    baseUrlApi: 'https://localhost:7077/api'
     // baseUrlApi: 'https://localhost:8009/api'
-    // baseUrlApi: 'http://192.168.1.181:2023/api' // public to server minhtd
+    // baseUrlApi: 'http://192.168.1.181:2023/api'
+    // baseUrlApi: 'https://dhtmapi.sfb.vn/api' // public to server minhtd
 };
+
